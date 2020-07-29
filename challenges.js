a0d692d65cf061e096e9871edcbfe049a4b95bd7
Frank Rauen <home@Franks-MacBook-Air.local>
2020-07-29 12:06:10 -0600

    solves charCount

--- challenges.js
+++ challenges.js
@@ -258,7 +258,18 @@ function sayHello() {
   -----------------------------------------------------------------*/
   // Your solution for 09-charCount here:
   
-  
+  function charCount(str) {
+    let result = {};
+    for(let i = 0; i < str.length; i++) {
+      let char = str.charAt(i);
+      if (result[char]) {
+        result[char]++;
+      } else {
+        result[char] = 1;
+      }
+    }
+    return result;
+  }
   
   
   
